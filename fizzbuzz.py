23b44faaab449ca683e114a98dae9271cdab1d90
shilarpl8-ui <shilarpl8@gmail.com>
2026-09-09 09:22:19 +0700

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+n=int(input("masukkan batas: "))
+i=1
+
+while i <=n:
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
+    i = i + 1
